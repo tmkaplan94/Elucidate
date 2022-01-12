5913718cd12aaf543339c8c41ffb43a1ad75341a
Jihigera <grantreed27@gmail.com>
2022-01-11 16:39:41 -0800

    added pickup system

---
+++ Assets/PickUpSystem.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpSystem : MonoBehaviour
+{
+    PlayerStats stats;
+    string itemTag = "Item";
+    string weaponTag = "Weapon";
+    [SerializeField]
+    Transform hand;
+    [SerializeField]
+    Transform dropPoint;
+
+
+
+
+    void Start()
+    {
+        stats = GetComponent<PlayerStats>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag(weaponTag))
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+                PickupWeapon(other.gameObject);
+            }
+        }
+    }  
+    
+    private void PickupWeapon(GameObject newWeapon)
+    {
+        stats.EquipWeapon(newWeapon);
+    }
+
+}
