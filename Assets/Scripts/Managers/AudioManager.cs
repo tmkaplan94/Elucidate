10d3383652d3a0a4e6edb3a4b4f7e6995edb93ef
Tyler Kaplan <tmkaplan94@gmail.com>
2022-01-22 09:44:03 -0800

    Created AudioManager

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,31 @@
+/*
+ * Version - 1.0
+ * Date - 01/22/2022
+ * Description - AudioManager managers all audio sources.
+ * Summary
+ *  - AudioManager extends Singleton, which extends MonoBehavior.
+ *  - contains a varying list of all sound sources
+ *  - responsible for finding and playing them
+ * 
+ * Author - Tyler Kaplan
+ * Contributors
+ *  - 
+ */
+
+using UnityEngine;
+using UnityEditor.Audio;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
