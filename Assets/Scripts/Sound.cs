e1b5f4f56d3d1d937db4e4085decbf3c21eec34d
Tyler Kaplan <tmkaplan94@gmail.com>
2022-01-22 09:54:14 -0800

    Created Sound class

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,20 @@
+/*
+ * Version - 1.0
+ * Date - 01/22/2022
+ * Description - Sound is a custom class used by the AudioManager.
+ * Summary
+ *  - Sound extends ScriptableObject.
+ * 
+ * Author - Tyler Kaplan
+ * Contributors
+ *  - 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : ScriptableObject
+{
+
+}
