b96d153158e8ce1e3fdb04aa49cfe144566ae73c
Tyler Kaplan <tmkaplan94@gmail.com>
2022-02-01 09:14:46 -0800

    Added PublishLossEventOnP.cs for testing

---
+++ Assets/Scripts/For Testing/PublishLossEventOnP.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PublishLossEventOnP : MonoBehaviour
+{
+    public class PublishWinEventOnP : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                GameEventBus.Publish(GameEvent.LOSS);
+            }
+        }
+    }
+}
