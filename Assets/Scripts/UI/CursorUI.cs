7f61acd831c2b8b7ec6705d56b42b40822c4efef
LocT02 <xxthegreeksxx@gmail.com>
2022-03-09 03:00:53 -0800

    added small clicking animation on cursor + crosshair
    - Idk if needs to be synced for network

--- Assets/Scripts/UI/CursorUI.cs
+++ Assets/Scripts/UI/CursorUI.cs
@@ -1,17 +1,26 @@
 /*
  * Author: Loc Trinh
  * Contributors: 
- * Description: Small script that handles custom cursor UI.
+ * Description: Small script that handles custom cursor UI and some special interactions.
  */
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CursorUI : MonoBehaviour
 {
     [SerializeField] private GameObject _crosshair;
     [SerializeField] private GameObject _cursor;
+
+    [SerializeField] private Sprite _unclicked;
+    [SerializeField] private Sprite _clicked;
+    [SerializeField] private RectTransform _scale;
+    [SerializeField] private float _scaleAmountBefore;
+    [SerializeField] private float _scaleAmountAfter;
+    private Image _image;
+
 
     void OnEnable()
     {
@@ -30,11 +39,40 @@ public class CursorUI : MonoBehaviour
         GameEventBus.Resume -= changeCrosshair;
 
     }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        _image = GetComponent<Image>();
+    }
     void Update()
     {
         Cursor.visible = false;
         Vector2 cursorPos = Input.mousePosition;
         transform.position = cursorPos;
+
+        if(Input.GetMouseButtonDown(0) && _cursor.activeSelf)
+        {
+            _image.sprite = _clicked;
+            _scale.localScale = new Vector3(_scaleAmountAfter, _scaleAmountAfter, 1.0f);
+        }
+        else if(Input.GetMouseButtonUp(0) && _cursor.activeSelf)
+        {
+            _image.sprite = _unclicked;
+            _scale.localScale = new Vector3(_scaleAmountBefore, _scaleAmountBefore, 1.0f);
+        }
+        
+        else if(Input.GetMouseButtonDown(0) && !_cursor.activeSelf)
+        {
+            _image.sprite = _clicked;
+            _scale.localScale = new Vector3(_scaleAmountAfter, _scaleAmountAfter, 1.0f);
+        }
+        else if(Input.GetMouseButtonUp(0) && !_cursor.activeSelf)
+        {
+            _image.sprite = _unclicked;
+            _scale.localScale = new Vector3(_scaleAmountBefore, _scaleAmountBefore, 1.0f);
+        }
+        
     }
 
     private void changeCursor()
